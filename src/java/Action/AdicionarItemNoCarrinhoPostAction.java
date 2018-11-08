fd3026c86c9f350001a6be10009e6b687f9a3e85
victorreis <v.crisostomo.reis@gmail.com>
2018-11-08 18:44:26 -0200

    Testes em adicionar item ao carrinho

--- src/java/Action/AdicionarItemNoCarrinhoPostAction.java
+++ src/java/Action/AdicionarItemNoCarrinhoPostAction.java
@@ -2,6 +2,7 @@ package Action;
 
 import Controller.Action;
 import Persistence.ItemDao;
+import Persistence.PedidoDao;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,19 +15,9 @@ public class AdicionarItemNoCarrinhoPostAction implements Action{
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession sessionScope = request.getSession();
         
-        Integer idRestaurante = Integer.parseInt((String) request.getParameter("id_restaurante"));
-        String nome = (String) request.getParameter("nome");
-        String tipo = (String) request.getParameter("tipo");
-        String descricao = (String) request.getParameter("descricao");
-        Double preco = Double.parseDouble(request.getParameter("preco"));
-        Integer disponivel = "Sim".equals((String) request.getParameter("disponivel")) ? 1 : 0;
-        Integer promocao = "Sim".equals((String) request.getParameter("promocao")) ? 1 : 0;
+        Integer quantidadeItem = Integer.parseInt((String) request.getParameter("quantidade_item"));
+        Integer idItem = Integer.parseInt((String) request.getParameter("id_item"));
         
-        if (ItemDao.getInstance().insertItem(nome, tipo, descricao, preco, disponivel, promocao, idRestaurante)) {
-            sessionScope.setAttribute("sucesso", "Item cadastrado com sucesso.");
-        } else {
-            sessionScope.setAttribute("erro", "Item não foi cadastrado.");
-        }
-        response.sendRedirect("MainServlet?parametro=CadastrarItem");
+        response.sendRedirect("MainServlet?parametro=ListarRestaurantes");
     }
 }
--- src/java/Persistence/PedidoDao.java
+++ src/java/Persistence/PedidoDao.java
@@ -9,6 +9,7 @@ import State.ActionFactoryState;
 import State.Pedido;
 import State.StatePedido;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,8 +25,58 @@ public class PedidoDao {
 
     private PedidoDao() {
     }
+    
     public static PedidoDao getInstance(){
         return instance;
+    }
+    
+    private void closeResoucers(Connection conn, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+
+        }
+
+    }
+    
+    public Boolean insertItemNoPedido(String nome, String tipo, String descricao, Double preco, Integer disponivel, Integer promocao, Integer idRestaurante) {
+//        Connection conn = null;
+//        PreparedStatement ps = null;
+//        ResultSet resultado;
+//
+//        if (idRestaurante != null
+//                && nome != null && tipo != null
+//                && descricao != null && preco != null
+//                && disponivel != null && promocao != null) {
+//            try {
+//                conn = DataBaseLocator.getInstance().getConnection();
+//
+//                ps = conn.prepareStatement("INSERT INTO item "
+//                        + "(nome, tipo, descricao, preco, disponivel, promocao, id_restaurante) "
+//                        + "VALUES(?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+//                ps.setString(1, nome);
+//                ps.setString(2, tipo);
+//                ps.setString(3, descricao);
+//                ps.setDouble(4, preco);
+//                ps.setInt(5, disponivel);
+//                ps.setInt(6, promocao);
+//                ps.setInt(7, idRestaurante);
+//
+//                ps.executeUpdate();
+//                resultado = ps.getGeneratedKeys();
+//            } catch (SQLException | ClassNotFoundException e) {
+//                Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+//            } finally {
+//                closeResoucers(conn, ps);
+//            }
+//        }
+        return true;
     }
     
     public void saveEstado(int id, String estado) {
@@ -111,22 +162,6 @@ public class PedidoDao {
             closeResoucers(conn, st);
         }
         return null;
-    }
-
-
-    private void closeResoucers(Connection conn, Statement st) {
-        try {
-            if (st != null) {
-                st.close();
-            }
-            if (conn != null) {
-                conn.close();
-            }
-
-        } catch (SQLException e) {
-
-        }
-
     }
 
     public HistoricoDeMementos getMementos(int id_pedido) {
